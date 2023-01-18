05884ddebc4683159c7c1a13d71f32b74d63e893
Samson Poon <samsonkfpoon@gmail.com>
2023-01-18 23:39:35 +0000

    initial shows up and logs a message when connecting to server

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const net = require('net');
-//establish connection with game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-// interpret incoming data as text
-  conn.setEncoding("utf8")
+const connect = require("./client");
 
-  conn.on("data", (data) => {
-    console.log(data)
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
-connect()
+connect();
